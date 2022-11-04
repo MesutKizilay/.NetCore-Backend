2530d806225269057305895c9dd7246ae8eef4e3
Mesut Kızılay <mesutkizilay3868@gmail.com>
2022-11-04 20:15:09 +0300

    Add project files.

--- WepAPI/Startup.cs
+++ WepAPI/Startup.cs
@@ -31,8 +31,8 @@ namespace WepAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddSingleton<IProductService, ProductManager>();
-            services.AddSingleton<IProductDal, EfProductDal>();
+            //services.AddSingleton<IProductService, ProductManager>();
+            //services.AddSingleton<IProductDal, EfProductDal>();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WepAPI", Version = "v1" });
