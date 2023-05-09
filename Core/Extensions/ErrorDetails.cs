b3d8ac7d6cde1ea90012f8ac186232d71309ab84
Mesut Kızılay <mesutkizilay3868@gmail.com>
2023-05-09 23:15:30 +0300

    ExceptionMiddleware was created for ValidationException.

--- Core/Extensions/ErrorDetails.cs
+++ Core/Extensions/ErrorDetails.cs
@@ -1,4 +1,7 @@
-﻿using Newtonsoft.Json;
+﻿using FluentValidation.Results;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Core.Extensions
 {
@@ -6,10 +9,15 @@ namespace Core.Extensions
     {
         public string Message { get; set; }
         public int StatusCode { get; set; }
-
+        
         public override string ToString()
         {
             return JsonConvert.SerializeObject(this);
         }
+    }
+
+    public class ValidationErrorDetail : ErrorDetails
+    {
+        public IEnumerable<ValidationFailure> Errors { get; set; }
     }
 }
